1266a1a8641dd5cfb27d746adacecbcd929dfb0a
Salida Maharjan <salidamaharjan@outlook.com>
2025-07-01 11:21:18 -0500

    Create Armor class

---
+++ src/app/Armor.java
@@ -0,0 +1,9 @@
+package app;
+
+public class Armor extends SalableProduct {
+
+	public Armor(String name, String description, double price, int quantity) {
+		super(name, description, price, quantity);
+	}
+	
+}
--- src/app/Weapon.java
+++ src/app/Weapon.java
@@ -1,5 +1,9 @@
 package app;
 
-public class Weapon {
+public class Weapon extends SalableProduct {
+
+	public Weapon(String name, String description, double price, int quantity) {
+		super(name, description, price, quantity);
+	}
 
 }
