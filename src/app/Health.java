13a078d383dd07edd6e6f88e9bc9d3c2dce103e0
Salida Maharjan <salidamaharjan@outlook.com>
2025-07-01 11:24:06 -0500

    Create Health class

---
+++ src/app/Health.java
@@ -0,0 +1,9 @@
+package app;
+
+public class Health extends SalableProduct {
+	
+	public Health(String name, String description, double price, int quantity) {
+		super(name, description, price, quantity);
+	}
+
+}
