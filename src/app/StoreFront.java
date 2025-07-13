454183cc0094ab9d5f4b049674db580854b8d29e
Salida Maharjan <salidamaharjan@outlook.com>
2025-07-13 13:48:51 -0500

    Add jackson library and define build path for the project

--- src/app/StoreFront.java
+++ src/app/StoreFront.java
@@ -7,7 +7,6 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-//import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
